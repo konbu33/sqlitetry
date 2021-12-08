64984fc74312c19d11df4efab66fdf1b3c7617f5
sample <sample@sample.com>
2021-12-08 12:45:48 +0900

    feat: DBのアラームデータ更新機能を実装

--- lib/pages/add_edit_alarm_page.dart
+++ lib/pages/add_edit_alarm_page.dart
@@ -59,7 +59,8 @@ class _AddEditAlarmPageState extends State<AddEditAlarmPage> {
                   alarmTime: DateTime(
                       2000, 1, 1, selectedDate.hour, selectedDate.minute));
               if (widget.index != null) {
-                widget.alarmList[widget.index!] = alarm;
+                alarm.id = widget.alarmList[widget.index!].id;
+                await DbProvider.updateData(alarm);
               } else {
                 await DbProvider.insertData(alarm);
               }
